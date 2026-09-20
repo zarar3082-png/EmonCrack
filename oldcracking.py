607ea6ba183213d373ca9eb67565a8d8b8108687
EMON 3200 <zarar3082@gmail.com>
2026-09-20 16:49:55 +0600

    Update oldcracking.py

--- oldcracking.py
+++ oldcracking.py
@@ -59,7 +59,8 @@ def fetch_allowed_ids(url):
 # 3. Security Check Function with Loading & Redirect
 # ----------------------------------------------------------------------------------------------
 def check_access():
-    RAW_GITHUB_URL = "https://github.com/zarar3082-png/EmonCrack/blob/main/.gitignore"
+    # RAW_GITHUB_URL = "https://github.com/zarar3082-png/EmonCrack/blob/main/.gitignore"
+    RAW_GITHUB_URL = "https://raw.githubusercontent.com/zarar3082-png/EmonCrack/main/.gitignore"
 
     user_id = get_permanent_device_id()
     os.system("clear")
